3879a8608002c787db015cc2edcf5571fe89c128
Quyet's Code <quyetcoccoc@gmail.com>
2024-10-29 18:18:24 +0700

    update gitignore

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,19 +2,19 @@
 </script>
 
 <template>
-  <section class="h-screen px-5 py-32 flex flex-col items-center justify-start bg-gray-50">
+  <section class="h-screen px-5 py-32 flex flex-col items-center justify-start">
     <h1 class="text-4xl font-bold text-orange-600 mb-4">Open a ticket!</h1>
-    <p class="text-lg text-gray-600 mb-8">Get stuck? Open a ticket now!</p>
-    <div class="space-x-4">
+    <p class="text-lg text-gray-100 mb-8">Get stuck? Open a ticket now!</p>
+    <div class="flex flex-col gap-y-3 sm:flex-row sm:space-x-4">
       <button
-        class="px-6 py-2 rounded-lg text-white bg-orange-600 hover:bg-orange-700 transition-colors duration-200 shadow-md">
+        class="min-w-[200px] px-6 py-2 rounded-lg text-white bg-orange-600 hover:bg-orange-700 transition-colors duration-200 shadow-md">
         <RouterLink to="/my-tickets">
           Open new ticket
         </RouterLink>
 
       </button>
       <button
-        class="px-6 py-2 rounded-lg text-orange-600 border border-orange-600 hover:bg-orange-100 transition-colors duration-200 shadow-md">
+        class="min-w-[200px] px-6 py-2 rounded-lg text-orange-600 border border-orange-600 hover:bg-orange-100 transition-colors duration-200 shadow-md">
         <RouterLink to="/create-ticket">
           View my tickets
         </RouterLink>
