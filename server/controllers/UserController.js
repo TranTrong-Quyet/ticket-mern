21df521cbd846b62d9f298f23c4869d936929c3c
Quyet's Code <quyetcoccoc@gmail.com>
2024-10-30 11:39:09 +0700

    add json web token

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,11 +1,13 @@
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
 const { genSalt } = bcrypt;
 
 //  Register user
 const registerUser = asyncHandler(async (req, res) => {
+  console.log("Request body:", req.body);
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -37,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -47,6 +50,33 @@ const registerUser = asyncHandler(async (req, res) => {
 // LOGIN UESR
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credential");
+  }
 });
 
-export { registerUser, loginUser };
+// Get current user
+// api/user/me
+// private
+const getMe = asyncHandler(async (req, res) => {
+  res.send("Me here");
+});
+
+// generate jsonwebtoken
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+export { registerUser, loginUser, getMe };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const UserRouter = express.Router();
-import { registerUser, loginUser } from "../controllers/UserController.js";
+import {
+  registerUser,
+  loginUser,
+  getMe,
+} from "../controllers/UserController.js";
 
 UserRouter.post("/", registerUser);
 
 UserRouter.post("/login", loginUser);
+UserRouter.get("/me", getMe);
 
 export default UserRouter;
